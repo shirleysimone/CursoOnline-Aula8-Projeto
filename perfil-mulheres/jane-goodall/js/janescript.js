c36ea39e37c6b4f6c942e69f3858f8322c5bf036
Caroline Jandoso <carol.jandoso@gmail.com>
2019-04-29 21:18:05 -0300

    adição de carousel e modal

--- perfil-mulheres/jane-goodall/js/janescript.js
+++
@@ -1,9 +0,0 @@
-var quoteJane = document.getElementById("quote");
-
-quoteJane.addEventListener("click", quoteJaneClick);
-
-console.log("testando");
-
-function quoteJaneClick() {
-    alert('You have clicked me!');
-}
