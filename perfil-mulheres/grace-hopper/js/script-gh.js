32acb9f0f39cc2a1fa1b17d113b6cbd8be013b81
Estrela <estrela.steinkirch@gmail.com>
2019-05-01 20:37:51 +0200

    Adiciona a sticky sidebar e consertos de layout

---
+++ perfil-mulheres/grace-hopper/js/script-gh.js
@@ -0,0 +1,21 @@
+let mainNavLinks = document.querySelectorAll("aside nav ul li a");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", () => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
