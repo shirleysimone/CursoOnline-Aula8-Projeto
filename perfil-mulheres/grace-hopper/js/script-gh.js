3190480df62f195575e6848578d43722e3aec782
Estrela <estrela.steinkirch@gmail.com>
2019-05-02 21:19:25 +0200

    Entrega do Projeto Final

--- perfil-mulheres/grace-hopper/js/script-gh.js
+++ perfil-mulheres/grace-hopper/js/script-gh.js
@@ -1,21 +1,31 @@
+// Nav ASIDE
+
 let mainNavLinks = document.querySelectorAll("aside nav ul li a");
 
 let lastId;
 let cur = [];
 
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll", event => {
   let fromTop = window.scrollY;
 
   mainNavLinks.forEach(link => {
     let section = document.querySelector(link.hash);
 
     if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
+      section.offsetTop - 170 <= fromTop &&
+      section.offsetTop + section.offsetHeight - 170 > fromTop
     ) {
       link.classList.add("current");
     } else {
       link.classList.remove("current");
     }
   });
+});
+
+// Ativar a explicação do BUG
+const botao = document.querySelector(".secao-diva__btn");
+
+botao.addEventListener("click", function() {
+  let resposta = document.querySelector(".secao-diva__bug--resposta");
+  resposta.classList.toggle("secao-diva__bug--desativada");
 });
